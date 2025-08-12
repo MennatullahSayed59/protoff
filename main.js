34ef3f64e4a2ad3fead376f800cb522b1486403a
MennatullahSayed59 <mennatullahsayed592@gmail.com>
2025-08-12 12:11:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function handelColorChange() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256); 
+
+    let color ="rgb(" + red + "," + green + "," + blue +")";
+
+    document.getElementById("ColorFlipper").style.backgroundColor = color ;
+}
